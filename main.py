0b4dac4a8e1470d398ddccc91f6fc959247b211d
Rajiviky <147384296+Rajiviky@users.noreply.github.com>
2025-11-26 11:17:01 +0000

    list unpacking

--- main.py
+++ main.py
@@ -152,3 +152,22 @@ print(bucket)
 bucket.reverse()
 print(bucket)
 
+# common list patterns  
+
+sentence  = " "
+new_sentence = sentence.join(['hi', 'my', 'name', 'is', 'joe'])
+print(new_sentence)
+
+# quick way would be 
+
+new_sentence = ' '.join(['hi', 'my', 'name', 'is', 'joe'])
+print(new_sentence)
+
+# list unpacking
+
+a, b, c, *other, d = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(a)
+print(b)
+print(c)
+print(other)
+print(d)
